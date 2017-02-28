35fb76269bcb64248a9fd051211b4b9f4bf97244
DavidScheers <david.scheers@gmail.com>
2017-02-28 10:33:40 +0100

    Implemented PerformanceManagementTool

--- src/main/PerformanceManagementTool.java
+++ src/main/PerformanceManagementTool.java
@@ -1,31 +1,48 @@
 package main;
 
+import main.Functions.DataBasedFunction;
+import main.Functions.FormFunction;
 import main.Functions.Function;
+import main.Functions.PerformanceFunction;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by davids on 28/02/2017.
  */
 public class PerformanceManagementTool implements PerformanceManagement {
 
+
+    private Map<LocalDate, Double> tssMap;
+    private Function tssFunction;
+
+    public PerformanceManagementTool() {
+        this.tssMap = new HashMap<>();
+        this.tssFunction = new DataBasedFunction(tssMap);
+    }
+
     @Override
     public void addTrainingsDay(LocalDate date, double tss) {
-
+        tssMap.put(date, tss);
     }
 
     @Override
     public Function getFitnessFunction() {
-        return null;
+        Function fitnessFunction = new PerformanceFunction(tssFunction,42, LocalDate.now());
+        return fitnessFunction;
     }
 
     @Override
     public Function getFatigueFunction() {
-        return null;
+        Function fatigueFunction = new PerformanceFunction(tssFunction,7, LocalDate.now());
+        return fatigueFunction;
     }
 
     @Override
     public Function getFormFunction() {
-        return null;
+        Function formFunction = new FormFunction(getFitnessFunction(), getFatigueFunction());
+        return formFunction;
     }
 }
