d491b66a6063fea7baef9d39c86c0825e9a62273
DavidScheers <david.scheers@gmail.com>
2017-03-01 13:07:17 +0100

    refactored getValue()

--- src/main/Functions/DataBasedFunction.java
+++ src/main/Functions/DataBasedFunction.java
@@ -10,16 +10,18 @@ public class DataBasedFunction implements Function {
 
     private Map<LocalDate, Double>  valueMap;
 
-    @Override
-    public double getValue(LocalDate inputDate) {
-        if (valueMap.containsKey(inputDate)) {
-            return valueMap.get(inputDate);
-        } else {
-            return 0;
-        }
-    }
-
     public DataBasedFunction(Map<LocalDate, Double> valueMap) {
         this.valueMap = valueMap;
+    }
+
+    @Override
+    public double getValue(LocalDate inputDate) {
+        double output;
+        if (valueMap.containsKey(inputDate)) {
+            output = valueMap.get(inputDate);
+        } else {
+            output = 0;
+        }
+        return output;
     }
 }
