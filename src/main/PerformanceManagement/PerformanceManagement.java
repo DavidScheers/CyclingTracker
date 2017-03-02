51492253610b4e8a4b555e5b0248837ffe9c43b8
DavidScheers <david.scheers@gmail.com>
2017-03-02 16:47:16 +0100

    Added methods to interface

--- src/main/PerformanceManagement/PerformanceManagement.java
+++ src/main/PerformanceManagement/PerformanceManagement.java
@@ -2,16 +2,23 @@ package main.PerformanceManagement;
 
 import main.Functions.Function;
 
+import java.time.Duration;
 import java.time.LocalDate;
 
 public interface PerformanceManagement {
 
-    void addTrainingsDay(LocalDate date, double tss);
+    void addTrainingsDay(LocalDate date, WorkoutIntensity intensity);
+    void addNewFtpMeasurement(LocalDate date, double ftp);
 
+    Function getFtpFunction();
     Function getFitnessFunction();
     Function getFatigueFunction();
 
     Function getFormFunction();
-
-
 }
+
+/*
+uw PMT zijn interface uitbreiden zodat ge niet direct TSS aangeeft,
+maar dat ge daar kunt zeggen: "ik heb een FTP test gedaan en dit was het resultat" en ook
+"ik heb X minuten getrained aan Y Watt (of whatever you need foor TSS te berekenen"
+ */
