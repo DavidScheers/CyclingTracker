2ce33b1eb3bd9f712d6094c5dd2bc8d66834abe7
DavidScheers <david.scheers@gmail.com>
2017-03-02 16:47:38 +0100

    Started working on implementation of new interface methods; WIP

--- src/main/PerformanceManagement/PerformanceManagementTool.java
+++ src/main/PerformanceManagement/PerformanceManagementTool.java
@@ -1,28 +1,41 @@
 package main.PerformanceManagement;
 
 import main.Functions.*;
+
+import java.time.Duration;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PerformanceManagementTool implements PerformanceManagement {
 
-    private Map<LocalDate, Double> tssMap;
+    private Map<LocalDate, WorkoutIntensity> tssMap;
+    private Map<LocalDate, Double> ftpMap;
     private LocalDate startOfTrackingDate;
 
     public PerformanceManagementTool(LocalDate startOfTrackingDate) {
         this.tssMap = new HashMap<>();
         this.startOfTrackingDate = startOfTrackingDate;
+        this.ftpMap = new HashMap<>();
     }
 
     @Override
-    public void addTrainingsDay(LocalDate date, double tss) {
+    public void addTrainingsDay(LocalDate date, WorkoutIntensity intensity) {
         if (tssMap.containsKey(date)) {
-            double temp = tssMap.get(date);
-            tssMap.put(date, temp+tss);
+            tssMap.put(date, getWorkoutIntensity(date, intensity));
         } else {
-            tssMap.put(date, tss);
+            tssMap.put(date, intensity);
         }
+    }
+
+    @Override
+    public void addNewFtpMeasurement(LocalDate date, double ftp) {
+        ftpMap.put(date, ftp);
+    }
+
+    @Override
+    public Function getFtpFunction() {
+        return new ZeroOrderHoldFunction(ftpMap);
     }
 
     @Override
@@ -45,4 +58,36 @@ public class PerformanceManagementTool implements PerformanceManagement {
         return new ShiftedFunction(differenceFunction, 1);
     }
 
+    private WorkoutIntensity getWorkoutIntensity(LocalDate date, WorkoutIntensity intensity) {
+        return new WorkoutIntensity(getNewDuration(date, intensity), getNewPower(date, intensity));
+    }
+
+    private double getNewPower(LocalDate date, WorkoutIntensity intensity) {
+        return getPower(date) + intensity.getPower();
+    }
+
+    private Duration getNewDuration(LocalDate date, WorkoutIntensity intensity) {
+        return getWorkoutDuration(date).plus(intensity.getWorkoutDuration());
+    }
+
+    private double getPower(LocalDate date) {
+        return tssMap.get(date).getPower();
+    }
+
+    private Duration getWorkoutDuration(LocalDate date) {
+        return tssMap.get(date).getWorkoutDuration();
+    }
+
 }
+
+/*
+    @Override
+    public void addTrainingsDay(LocalDate date, double tss) {
+        if (tssMap.containsKey(date)) {
+            double temp = tssMap.get(date);
+            tssMap.put(date, temp+tss);
+        } else {
+            tssMap.put(date, tss);
+        }
+    }
+ */
