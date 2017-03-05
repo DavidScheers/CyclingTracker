ab649ab93c10a3824a73693cf7c4a749e50cd496
David Scheers <david.scheers@gmail.com>
2017-03-05 16:27:04 +0100

    Added function addListener to Function interface

--- src/main/Functions/Function.java
+++ src/main/Functions/Function.java
@@ -1,9 +1,12 @@
 package main.Functions;
 
+import main.Listener.FunctionListener;
+
 import java.time.LocalDate;
 
 public interface Function {
 
     double getValue(LocalDate date);
+    void addListener(FunctionListener functionListener);
 }
 
