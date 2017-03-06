7b0a3d4e0c2a08393238722cddaab43cf42bfe03
DavidScheers <david.scheers@gmail.com>
2017-03-06 17:01:24 +0100

    refactored because of changes in DataBasedFunction

--- src/test/PerformanceFunctionTest.java
+++ src/test/PerformanceFunctionTest.java
@@ -7,8 +7,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,8 +16,7 @@ import static org.junit.Assert.assertEquals;
 public class PerformanceFunctionTest {
 
     private Function performanceFunction;
-    private Function tssFunction;
-    private Map<LocalDate, Double> valueMap;
+    private DataBasedFunction tssFunction;
     private final LocalDate TODAY = LocalDate.now();
     private final LocalDate YESTERDAY = LocalDate.now().minusDays(1);
     private final LocalDate YESTERYESTERDAY = LocalDate.now().minusDays(2);
@@ -28,27 +25,26 @@ public class PerformanceFunctionTest {
 
     @Before
     public void setUp() throws Exception {
-        valueMap = new HashMap<>();
+        tssFunction = new DataBasedFunction();
         getData();
-        tssFunction = new DataBasedFunction(valueMap);
         performanceFunction = new PerformanceFunction(tssFunction, 42, LONG_AGO);
     }
 
     private void getData() {
-        valueMap.put(TODAY, 0.55);
-        valueMap.put(YESTERYESTERDAY, 0.4);
-        valueMap.put(YESTERDAY, 0.8);
-        valueMap.put(ONE_WEEK_AGO, 0.35);
+        tssFunction.addValue(TODAY, 0.55);
+        tssFunction.addValue(YESTERYESTERDAY, 0.4);
+        tssFunction.addValue(YESTERDAY, 0.8);
+        tssFunction.addValue(ONE_WEEK_AGO, 0.35);
     }
 
     @Test
     public void getValue_Today_ShouldReturn() throws Exception {
-        assertEquals(performanceFunction.getValue(TODAY), 0.04029, 0.00001);
+        assertEquals(performanceFunction.getValue(TODAY), 0.04726, 0.00001);
     }
 
     @Test
     public void getValue_StartDateBeforeFirstTraining() throws Exception {
-        assertEquals(performanceFunction.getValue(ONE_WEEK_AGO), 0.008234, 0.0001);
+        assertEquals(performanceFunction.getValue(LONG_AGO.minusDays(1)), 0.0, 0.0001);
     }
 
 }
