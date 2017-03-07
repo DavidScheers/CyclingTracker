c2486127d3897df101c50740484e11f8e2584744
DavidScheers <david.scheers@gmail.com>
2017-03-07 14:32:55 +0100

    Added ListenerTest

--- src/test/java/DifferenceFunctionTest.java
+++ src/test/java/DifferenceFunctionTest.java
@@ -1,16 +1,18 @@
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import Functions.DataBasedFunction;
 import Functions.DifferenceFunction;
 import Functions.Function;
+import Listener.FunctionListener;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by davids on 28/02/2017.
- */
+
 public class DifferenceFunctionTest {
 
     private Function differenceFunction;
@@ -64,4 +66,20 @@ public class DifferenceFunctionTest {
         assertEquals(differenceFunction.getValue(LONG_AGO), 20, 0.0001);
     }
 
+    @Test
+    public void afterRegisteringListenerOnFirstFunction_ListenerGetsCalledWhenChangeIsDetected() throws Exception {
+        FunctionListener mockedFunctionListener = Mockito.mock(FunctionListener.class);
+        firstFunction.addListener(mockedFunctionListener);
+        firstFunction.addValue(ONE_WEEK_AGO.plusDays(1), 5.0);
+        verify(mockedFunctionListener).changeDetected();
+    }
+
+    @Test
+    public void afterRegisteringListenerOnSecondFunction_ListenerGetsCalledWhenChangeIsDetected() throws Exception {
+        FunctionListener mockedFunctionListener = Mockito.mock(FunctionListener.class);
+        secondFunction.addListener(mockedFunctionListener);
+        secondFunction.addValue(ONE_WEEK_AGO.plusDays(1), 5.0);
+        verify(mockedFunctionListener).changeDetected();
+    }
+// Dus ge wilt testen dat als ge een listener registreert dat die wordt aangeroepen als uw functie wijzigt
 }
