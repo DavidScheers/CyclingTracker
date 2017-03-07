3d800caa10ad7eee781f7d866865b2e15d380fbc
DavidScheers <david.scheers@gmail.com>
2017-03-07 14:32:04 +0100

    Implemented FunctionBaseImplementation

---
+++ src/main/java/Functions/FunctionBaseImplementation.java
@@ -0,0 +1,26 @@
+package Functions;
+
+import Listener.FunctionListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class FunctionBaseImplementation implements Function {
+
+    public List<FunctionListener> listeners;
+
+    public FunctionBaseImplementation() {
+        this.listeners = new ArrayList<>();
+    }
+
+    void notifyListeners(List<FunctionListener> listeners) {
+        for (FunctionListener listener : listeners) {
+            listener.changeDetected();
+        }
+    }
+
+    @Override
+    public void addListener(FunctionListener functionListener) {
+        listeners.add(functionListener);
+    }
+}
